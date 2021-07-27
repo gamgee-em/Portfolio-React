d01b72a2d9e21a76b1ddd2159bdde585e607bfae
Sam Sweigart <samuel.sweigart@gmail.com>
2021-07-27 13:04:36 -0400

    added # & email to contact page

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -2,6 +2,8 @@ import './index.css';
 
 const social = 
         <section className="social-icons">
+            <p className="contact-number">267.666.8499</p>
+           
             <a href="https://github.com/gamgee-em" rel="noreferrer" target='_blank' className="github">
                 <img src="https://img.icons8.com/material-two-tone/24/000000/github.png" alt="GitHub Icon"/>
             </a>
@@ -12,6 +14,7 @@ const social =
                 <img src="https://img.icons8.com/material-two-tone/24/000000/stack-of-tweets.png" alt="Twitter Icon"/>
             </a>
             
+            <p className="contact-email"><a href="mailto:samuel.sweigart@gmail.com">samuel.sweigart@gmail.com</a></p>          
             
         </section>;
 
