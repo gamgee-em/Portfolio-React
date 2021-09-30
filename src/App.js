fbebb9bd60e161a98891f79f9c2c7287d3abc6f8
Sam Sweigart <samuel.sweigart@gmail.com>
2021-09-30 16:58:40 -0400

    moved Router from App.js to index.js for use w/ AnimatePresence

--- src/App.js
+++ src/App.js
@@ -7,33 +7,38 @@ import Portfolio from './components/Portfolio/Portfolio';
 import Resume from './components/Resume/Resume';
 import Contact from './components/Contact/Contact';
 //! import and set alias 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 const App = () => {
+  
+  const location = useLocation();
+
   return (
-    <Router>
+    
       <div className="App background-image main-container">
         <Navbar />
-        <Switch>
-          <Route exact path={ ['/','/Portfolio-React'] }>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route exact path='/portfolio'>
-            <Portfolio />
-          </Route>
-          <Route exact path='/resume'>
-            <Resume />
-          </Route>
-          <Route exact path='/contact'>
-            <Contact />
-          </Route>
-        </Switch>
+        <AnimatePresence>
+          <Switch location={ location } key={ location.key }>
+            <Route exact path={ ['/','/Portfolio-React'] }>
+              <Home />
+            </Route>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/portfolio'>
+              <Portfolio />
+            </Route>
+            <Route exact path='/resume'>
+              <Resume />
+            </Route>
+            <Route exact path='/contact'>
+              <Contact />
+            </Route>
+          </Switch>
+        </AnimatePresence>
         <Footer />
       </div>
-    </Router>
   );
 }
 
